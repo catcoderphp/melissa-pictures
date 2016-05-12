7c72285eddb672a0c223ec0fe199c55c078ad9ca
catcoder <catcoder>
2016-05-12 13:14:04 -0500

    css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    <title>ALBUM MELISSA</title>
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
@@ -19,6 +19,7 @@
     <!-- Styles -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+
 
     <style>
         body {
@@ -29,76 +30,99 @@
             margin-right: 6px;
         }
     </style>
+    <link rel="stylesheet" href="{{asset('/css/main.css')}}"/>
 </head>
 <body id="app-layout">
-<nav class="navbar navbar-default navbar-static-top">
+<div class="text">
+    <svg>
+        <defs>
+            <mask id="mask" x="0" y="0" width="100%" height="100%" >
+                <!-- alpha rectangle -->
+                <!-- rectángulo alfa -->
+                <rect id="alpha" x="0" y="0" width="100%" height="100%"/>
+                <!-- All text that you want -->
+                <!-- Coloca todo el texto que necesites -->
+                <text id="title" x="50%" y="0" dy="1.58em">MELISSA</text>
+                <text id="subtitle" x="50%" y="0" dy="9.8em">PHOTOS</text>
+            </mask>
+        </defs>
+        <!-- Apply color here! -->
+        <!-- Color aquí -->
+        <rect id="base" x="0" y="30" width="100%" height="70%"></rect>
+    </svg>
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <!-- Branding Image -->
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    Melissa
+                </a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <!-- Left Side Of Navbar -->
+                <ul class="nav navbar-nav">
+                    <li><a href="{{ url('/home') }}">Home</a></li>
+                </ul>
+                @include('layouts.partials.nav')
+
+                        <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    @if (Auth::guest())
+                        <li><a href="{{ url('/login') }}">Login</a></li>
+                        <li><a href="{{ url('/register') }}">Register</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+    </nav>
     <div class="container">
-        <div class="navbar-header">
-
-            <!-- Collapsed Hamburger -->
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                <span class="sr-only">Toggle Navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-
-            <!-- Branding Image -->
-            <a class="navbar-brand" href="{{ url('/') }}">
-                Melissa
-            </a>
-        </div>
-
-        <div class="collapse navbar-collapse" id="app-navbar-collapse">
-            <!-- Left Side Of Navbar -->
-            <ul class="nav navbar-nav">
-                <li><a href="{{ url('/home') }}">Home</a></li>
-            </ul>
-            @include('layouts.partials.nav')
-
-                    <!-- Right Side Of Navbar -->
-            <ul class="nav navbar-nav navbar-right">
-                <!-- Authentication Links -->
-                @if (Auth::guest())
-                    <li><a href="{{ url('/login') }}">Login</a></li>
-                    <li><a href="{{ url('/register') }}">Register</a></li>
-                @else
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
-
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                        </ul>
-                    </li>
-                @endif
-            </ul>
-        </div>
+        @if (Session::has('errors'))
+            <div class="alert alert-warning" role="alert">
+                <ul>
+                    <strong>Oops! Something went wrong : </strong>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        @if (Session::has('success'))
+            <div class="alert alert-success" role="alert">
+                <ul>
+                    <strong>Success! Your action has been correct: </strong>
+                    <li>{{ Session::get('success') }}</li>
+                </ul>
+            </div>
+        @endif
     </div>
-</nav>
-<div class="container">
-    @if (Session::has('errors'))
-        <div class="alert alert-warning" role="alert">
-            <ul>
-                <strong>Oops! Something went wrong : </strong>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
-    @if (Session::has('success'))
-        <div class="alert alert-success" role="alert">
-            <ul>
-                <strong>Success! Your action has been correct: </strong>
-                <li>{{ Session::get('success') }}</li>
-            </ul>
-        </div>
-    @endif
+    @yield('content')
 </div>
 
-@yield('content')
+<section class="intro"></section>
+
+
+
 
         <!-- JavaScripts -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.3/jquery.min.js" integrity="sha384-I6F5OKECLVtK/BL+8iSLDEHowSAfUo76ZL9+kGAgTRdiByINKJaqTPH/QVNS1VDb" crossorigin="anonymous"></script>
--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -1,12 +1,12 @@
 @if(Auth::check())
     <ul class="nav navbar-nav">
         <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
+            <a id ="subtitle" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button"
                aria-expanded="false">Fotos <span class="caret"></span>
             </a>
             <ul class="dropdown-menu" role="menu">
-                <li><a href="{{route('photos.create')}}">Crear nueva foto</a></li>
-                <li><a href="{{route('photos.index')}}">Todas las fotos</a></li>
+                <li><a id="subtitle"href="{{route('photos.create')}}">Crear nueva foto</a></li>
+                <li><a id ="subtitle" href="{{route('photos.index')}}">Todas las fotos</a></li>
             </ul>
         </li>
     </ul>
