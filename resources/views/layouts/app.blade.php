77b27adf0a73c63a05c9d65f4933864e339023ff
catcoder <catcoder>
2016-05-12 13:57:32 -0500

    mejoras

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,84 +43,86 @@
                 <!-- All text that you want -->
                 <!-- Coloca todo el texto que necesites -->
                 <text id="title" x="50%" y="0" dy="1.58em">MELISSA</text>
-                <text id="subtitle" x="50%" y="0" dy="9.8em">PHOTOS</text>
+                <text id="subtitle" x="50%" y="0" dy="7.8em">PHOTOS</text>
             </mask>
         </defs>
         <!-- Apply color here! -->
         <!-- Color aquí -->
-        <rect id="base" x="0" y="30" width="100%" height="70%"></rect>
+        <rect id="base" x="0" y="30" width="100%" height="50%"></rect>
     </svg>
-    <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-            <div class="navbar-header">
 
-                <!-- Collapsed Hamburger -->
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                    <span class="sr-only">Toggle Navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-
-                <!-- Branding Image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    Melissa
-                </a>
-            </div>
-
-            <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                <!-- Left Side Of Navbar -->
-                <ul class="nav navbar-nav">
-                    <li><a href="{{ url('/home') }}">Home</a></li>
-                </ul>
-                @include('layouts.partials.nav')
-
-                        <!-- Right Side Of Navbar -->
-                <ul class="nav navbar-nav navbar-right">
-                    <!-- Authentication Links -->
-                    @if (Auth::guest())
-                        <li><a href="{{ url('/login') }}">Login</a></li>
-                        <li><a href="{{ url('/register') }}">Register</a></li>
-                    @else
-                        <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }} <span class="caret"></span>
-                            </a>
-
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
-            </div>
-        </div>
-    </nav>
-    <div class="container">
-        @if (Session::has('errors'))
-            <div class="alert alert-warning" role="alert">
-                <ul>
-                    <strong>Oops! Something went wrong : </strong>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-        @if (Session::has('success'))
-            <div class="alert alert-success" role="alert">
-                <ul>
-                    <strong>Success! Your action has been correct: </strong>
-                    <li>{{ Session::get('success') }}</li>
-                </ul>
-            </div>
-        @endif
-    </div>
-    @yield('content')
 </div>
 
-<section class="intro"></section>
+<section class="intro">
 
+</section>
+<nav class="navbar navbar-default navbar-static-top">
+    <div class="container">
+        <div class="navbar-header">
+
+            <!-- Collapsed Hamburger -->
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                <span class="sr-only">Toggle Navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+            <!-- Branding Image -->
+            <a class="navbar-brand" href="{{ url('/') }}">
+                Melissa
+            </a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="app-navbar-collapse">
+            <!-- Left Side Of Navbar -->
+            <ul class="nav navbar-nav">
+                <li><a href="{{ url('/home') }}">Home</a></li>
+            </ul>
+            @include('layouts.partials.nav')
+
+                    <!-- Right Side Of Navbar -->
+            <ul class="nav navbar-nav navbar-right">
+                <!-- Authentication Links -->
+                @if (Auth::guest())
+                    <li><a href="{{ url('/login') }}">Login</a></li>
+                    <li><a href="{{ url('/register') }}">Register</a></li>
+                @else
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                        </ul>
+                    </li>
+                @endif
+            </ul>
+        </div>
+    </div>
+</nav>
+<div class="container">
+    @if (Session::has('errors'))
+        <div class="alert alert-warning" role="alert">
+            <ul>
+                <strong>Oops! Something went wrong : </strong>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    @if (Session::has('success'))
+        <div class="alert alert-success" role="alert">
+            <ul>
+                <strong>Success! Your action has been correct: </strong>
+                <li>{{ Session::get('success') }}</li>
+            </ul>
+        </div>
+    @endif
+</div>
+@yield('content')
 
 
 
