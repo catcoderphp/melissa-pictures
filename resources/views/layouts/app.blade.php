1440cb86ff7fabe729db37189cb711f3f66631ca
catcoder <catcoder>
2016-05-12 23:09:34 -0500

    sin imagenes chafas

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -83,20 +83,7 @@
                 <div class="4u 12u(medium)">
 
                     <!-- Sidebar -->
-                    <div id="sidebar">
-                        <section class="widget thumbnails">
-                            <h3>Interesting stuff</h3>
-                            <div class="grid">
-                                <div class="row 50%">
-                                    <div class="6u"><a href="#" class="image fit"><img src="{{asset('images/pic04.jpg')}}" alt="" /></a></div>
-                                    <div class="6u"><a href="#" class="image fit"><img src="{{asset('images/pic05.jpg')}}" alt="" /></a></div>
-                                    <div class="6u"><a href="#" class="image fit"><img src="{{asset('images/pic06.jpg')}}" alt="" /></a></div>
-                                    <div class="6u"><a href="#" class="image fit"><img src="{{asset('images/pic01.jpg')}}" alt="" /></a></div>
-                                </div>
-                            </div>
-                            <a href="#" class="button icon fa-file-text-o">More</a>
-                        </section>
-                    </div>
+
 
                 </div>
                 <div class="8u 12u(medium) important(medium)">
