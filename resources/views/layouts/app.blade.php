4fdda30725320d6003f0ac3fddb704abd8fcdd49
catcoder <catcoder>
2016-05-12 19:27:07 -0500

    reglas google

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -204,10 +204,8 @@
 <script src="{{asset('assets/js/jquery.min.js')}}"></script>
 <script src="{{asset('assets/js/jquery.dropotron.min.js')}}"></script>
 <script src="{{asset('assets/js/skel.min.js')}}"></script>
-<script src="{{asset('assets/js/util.js')}}"></script>
+<script src="{{asset('assets/js/compress.js')}}"></script>
 <!--[if lte IE 8]><script src="{{asset('assets/js/ie/respond.min.js')}}"></script><![endif]-->
-<script src="{{asset('assets/js/main.js')}}"></script>
-<script src="{{asset('/assets/js/actions.js')}}"></script>
 
 </body>
 </html>
