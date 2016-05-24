bf0c0be72a0fd7da77627b03e98d79483348fb79
catcoder <catcoder.php@gmail.com>
2016-05-24 16:38:18 -0500

    form to create album

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -8,6 +8,11 @@ use App\Http\Requests;
 
 class AlbumController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -15,7 +20,7 @@ class AlbumController extends Controller
      */
     public function index()
     {
-        //
+        //return view('');
     }
 
     /**
@@ -36,7 +41,7 @@ class AlbumController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        dd($request->all());
     }
 
     /**
